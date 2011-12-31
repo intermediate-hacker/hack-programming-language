e3172f618f3993f0691fcc14e688f4dcd5ed6970
smat1995 <smat1995@gmail.com>
2011-12-31 12:12:17 +0000

    New pro_compiler folder.

---
+++ pro_compiler/Parser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace pro_compiler
+{
+    public class Parser
+    {
+        public List<string> result;
+
+        public Parser()
+        {
+            Output = new List<string>();
+            Result = new List<string>();
+        }
+
+        public List<string> Output
+        {
+            get;
+            set;
+        }
+
+        public List<string> Result
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ pro_compiler/Scanner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace pro_compiler
+{
+    class Scanner
+    {
+        List<object> result;
+        public Scanner(TextReader input)
+        {
+            result = new List<object>();
+
+        }
+
+        public void Scan(TextReader input)
+        {
+            while(input.Peek() != -1)
+            {
+                char ch = (char)input.Peek();
+
+                if(char.IsWhiteSpace(ch))
+                {
+                    input.Read();
+                }
+
+                else if(char.IsLetter(ch) || ch == '_')
+                {
+                    StringBuilder accum = new StringBuilder();
+                        
+                    input.Read(); // skip the '"'
+
+                    if(input.Peek() ==  -1)
+                    {
+                        throw new Exception("unterminated string literal");
+                    }
+
+                    while((ch = (char)input.Peek()) != '"')
+                    {
+                        accum.Append(ch);
+                        input.Read();
+
+                        if(input.Peek() == -1)
+                        {
+                            throw new Exception("unterminated string literal");
+                        }
+                    }
+
+                    //skip the terminating '"'
+                    input.Read();
+                    result.Add(accum);
+                }
+            }
+        }
+    }
+}
